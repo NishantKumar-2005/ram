ff444074574717043343a628acec6c585f1a5509
NishantKumar-2005 <120775351+NishantKumar-2005@users.noreply.github.com>
2024-01-14 12:28:21 +0530

    Ram

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='body'>
+        <div className='SupH'>
+        <span className="text-wrapper">जय श्री राम</span>
+            </div>
+        <div className='Heading'>
+          <p>राम लल्ला के प्रतिष्ठा समारोह के शुभ अवसर पर हार्दिक शुभकामनाएं!</p>
+        </div>
+        <div className='Mandir_img'></div>
+        <div className='tilak'></div>
+        <div className='Meassage'>
+          <p>आपको नए आदर्श राम लल्ला के प्रतिष्ठा समारोह के शुभ अवसर पर हार्दिक शुभकामनाएं! इस पवित्र क्षण में भगवान श्रीराम की कृपा और आशीर्वाद सदैव आपके साथ रहें। समृद्धि, शांति, और सद्गुण से भरपूर नये युग की शुरुआत हो।</p>
+        </div>
+        <div className='Sharelink'>
+          <div className='Wlogo'></div>
+          <div className='whatslink'>
+            <a href=""> SHARE LINK</a></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
